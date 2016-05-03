0575db9b2ba22cfa1604c13897d32ada9cf5a315
suvradip saha <suvradip@fusioncharts.com>
2016-05-03 19:07:28 +0530

    - a new feature setChartparameter is implemented

--- src/java/fusioncharts/FusionCharts.java
+++ src/java/fusioncharts/FusionCharts.java
@@ -4,23 +4,26 @@
  * and open the template in the editor.
  */
 package fusioncharts;
+import java.util.*;
 
 public class FusionCharts {
     private String constructorTemplate = "<script type=\"text/javascript\">FusionCharts.ready(function () {new FusionCharts(__constructorOptions__);});</script>";
-    private String renderTemplate = "<script type=\"text/javascript\">FusionCharts.ready(function () {                FusionCharts(\"__chartId__\").render();});</script>";
-    private String[] chartOptions = new String[10];
+    private String renderTemplate = "<script type=\"text/javascript\">FusionCharts.ready(function () {FusionCharts(\"__chartId__\").render();});</script>";
     private String chartDataSource = "";
+    private Map<String, String> chartOptions = new HashMap<String, String>();
+    
     public FusionCharts(String type, String id, String width, String height, String renderAt, String dataFormat, String dataSource) {
-        this.chartOptions[0] = id;
-        this.chartOptions[1] = width;
-        this.chartOptions[2] = height;
-        this.chartOptions[3] = renderAt;
-        this.chartOptions[4] = type;
-        this.chartOptions[5] = dataFormat;
-        if(this.chartOptions[5].contains("url")) {
-            this.chartOptions[6] = "\""+dataSource+"\"";
+        this.chartOptions.put("id", id);
+        this.chartOptions.put("width", width);
+        this.chartOptions.put("height", height);
+        this.chartOptions.put("renderAt", renderAt);
+        this.chartOptions.put("type", type);
+        this.chartOptions.put("dataFormat", dataFormat);
+        
+        if(dataFormat.contains("url")) {
+            this.chartOptions.put("dataSource", "\""+dataSource+"\"");
         } else {
-            this.chartOptions[6] = "__dataSource__";
+            this.chartOptions.put("dataSource", "__dataSource__");
             this.chartDataSource = this.addSlashes(dataSource.replaceAll("\n", ""));
         }
     }
@@ -39,21 +42,50 @@ public class FusionCharts {
         str = str.replaceAll("\\}\"\\}", "}}");
         return str;
     }
-    private String jsonEncode(String[] data){
-        String json = "{type: \""+this.chartOptions[4]+"\",renderAt: \""+this.chartOptions[3]+"\",width: \""+this.chartOptions[1]+"\",height: \""+this.chartOptions[2]+"\",dataFormat: \""+this.chartOptions[5]+"\",id: \""+this.chartOptions[0]+"\",dataSource: "+this.chartOptions[6]+"}";
+    private String jsonEncode(){
+        String json = 
+                "{type: \""+this.chartOptions.get("type")+
+                "\",renderAt: \""+this.chartOptions.get("renderAt")+
+                "\",width: \""+this.chartOptions.get("width")+
+                "\",height: \""+this.chartOptions.get("height")+
+                "\",dataFormat: \""+this.chartOptions.get("dataFormat")+
+                "\",id: \""+this.chartOptions.get("id")+
+                "\",dataSource: "+this.chartOptions.get("dataSource")+
+                "}";
         return json;
     }
     public String render() {
         String outputHTML;
-        if(this.chartOptions[5].contains("url")) {
-            outputHTML = this.constructorTemplate.replace("__constructorOptions__", this.jsonEncode(this.chartOptions))+this.renderTemplate.replace("__chartId__", this.chartOptions[0]);
+        if(this.chartOptions.get("dataFormat").contains("url")) {
+            outputHTML = this.constructorTemplate.replace("__constructorOptions__", 
+                this.jsonEncode())+this.renderTemplate.replace("__chartId__", 
+                this.chartOptions.get("id"));
         } else {
-            if("json".equals(this.chartOptions[5])) {
-                outputHTML = this.constructorTemplate.replace("__constructorOptions__", this.jsonEncode(this.chartOptions).replace("__dataSource__",this.chartDataSource))+this.renderTemplate.replace("__chartId__", this.chartOptions[0]);
+            if("json".equals(this.chartOptions.get("dataFormat"))) {
+                outputHTML = this.constructorTemplate.replace("__constructorOptions__",
+                    this.jsonEncode().replace("__dataSource__",
+                    this.chartDataSource))+this.renderTemplate.replace("__chartId__",
+                    this.chartOptions.get("id"));
             } else {
-                outputHTML = this.constructorTemplate.replace("__constructorOptions__", this.jsonEncode(this.chartOptions).replace("__dataSource__","\'"+this.chartDataSource+"\'"))+this.renderTemplate.replace("__chartId__", this.chartOptions[0]);
+                outputHTML = this.constructorTemplate.replace("__constructorOptions__", 
+                    this.jsonEncode().replace("__dataSource__","\'"+
+                    this.chartDataSource+"\'"))+
+                    this.renderTemplate.replace("__chartId__", this.chartOptions.get("id"));
             }
         }
         return outputHTML;
+    }
+    
+    public void setChartParameter(String param_key, String param_value){
+        if(param_key.equals("dataSource")){
+            if(param_value.contains("url")) {
+                this.chartOptions.put("dataSource", param_value);
+            } else {
+                this.chartOptions.put("dataSource", "__dataSource__");
+                this.chartDataSource = this.addSlashes(param_value.replaceAll("\n", ""));
+            }
+        } else {
+            this.chartOptions.put(param_key, param_value);
+        } 
     }
 }
