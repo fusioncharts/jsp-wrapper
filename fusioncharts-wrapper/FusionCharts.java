5f1679ae38b8a41cb89042f545e90030dd3a2e36
Poushali Guha <poushali@collabion.com>
2018-08-24 16:26:38 +0530

    add events and message customization support in java source file

--- fusioncharts-wrapper/FusionCharts.java
+++ fusioncharts-wrapper/FusionCharts.java
@@ -6,10 +6,14 @@
 package fusioncharts;
 
 public class FusionCharts {
-    private String constructorTemplate = "<script type=\"text/javascript\">FusionCharts.ready(function () {new FusionCharts(__constructorOptions__);});</script>";
-    private String renderTemplate = "<script type=\"text/javascript\">FusionCharts.ready(function () {                FusionCharts(\"__chartId__\").render();});</script>";
-    private String[] chartOptions = new String[10];
+	private String constructorTemplate = "<script type=\"text/javascript\">\nFusionCharts.ready(function () {\n\tvar fc_chart = new FusionCharts(%s);\n%sfc_chart.render();\n});\n</script>";
+	private String eventTemplate = "    fc_chart.addEventListener(\"%s\",%s);\n";
+	private String messageTemplate = ",%s:\"%s\"";
+	private String[] chartOptions = new String[10];
     private String chartDataSource = "";
+    private String fusionChartsEvents = "";
+    private String fusionChartsMessages = "";
+    
     public FusionCharts(String type, String id, String width, String height, String renderAt, String dataFormat, String dataSource) {
         this.chartOptions[0] = id;
         this.chartOptions[1] = width;
@@ -17,13 +21,16 @@ public class FusionCharts {
         this.chartOptions[3] = renderAt;
         this.chartOptions[4] = type;
         this.chartOptions[5] = dataFormat;
+        //this.chartOptions[6] = "%s"; 
         if(this.chartOptions[5].contains("url")) {
-            this.chartOptions[6] = "\""+dataSource+"\"";
+            this.chartOptions[7] = "\""+dataSource+"\"";
         } else {
-            this.chartOptions[6] = "__dataSource__";
+        	this.chartOptions[7] = "%s";
             this.chartDataSource = this.addSlashes(dataSource.replaceAll("\n", ""));
         }
+        
     }
+    
     private String addSlashes(String str) {
         str = str.replaceAll("\\\\", "\\\\\\\\");
         str = str.replaceAll("\\n", "\\\\n");
@@ -39,21 +46,39 @@ public class FusionCharts {
         str = str.replaceAll("\\}\"\\}", "}}");
         return str;
     }
+    
     private String jsonEncode(String[] data){
-        String json = "{type: \""+this.chartOptions[4]+"\",renderAt: \""+this.chartOptions[3]+"\",width: \""+this.chartOptions[1]+"\",height: \""+this.chartOptions[2]+"\",dataFormat: \""+this.chartOptions[5]+"\",id: \""+this.chartOptions[0]+"\",dataSource: "+this.chartOptions[6]+"}";
+        String json = "{type: \""+this.chartOptions[4]+"\",renderAt: \""+this.chartOptions[3]+"\",width: \""+this.chartOptions[1]+"\",height: \""+this.chartOptions[2]+"\",dataFormat: \""+this.chartOptions[5] + "\"," + 
+        				"id:\"" + this.chartOptions[0] + "\"" + this.fusionChartsMessages + ",dataSource: "+this.chartOptions[7]+"}";
         return json;
     }
+    
     public String render() {
         String outputHTML;
         if(this.chartOptions[5].contains("url")) {
-            outputHTML = this.constructorTemplate.replace("__constructorOptions__", this.jsonEncode(this.chartOptions))+this.renderTemplate.replace("__chartId__", this.chartOptions[0]);
+            outputHTML = String.format(this.constructorTemplate, this.jsonEncode(this.chartOptions));
+            
         } else {
             if("json".equals(this.chartOptions[5])) {
-                outputHTML = this.constructorTemplate.replace("__constructorOptions__", this.jsonEncode(this.chartOptions).replace("__dataSource__",this.chartDataSource))+this.renderTemplate.replace("__chartId__", this.chartOptions[0]);
+            	this.chartOptions[7] = String.format(this.chartOptions[7], this.chartDataSource);
+            	outputHTML = String.format(this.constructorTemplate, this.jsonEncode(this.chartOptions), fusionChartsEvents);
             } else {
-                outputHTML = this.constructorTemplate.replace("__constructorOptions__", this.jsonEncode(this.chartOptions).replace("__dataSource__","\'"+this.chartDataSource+"\'"))+this.renderTemplate.replace("__chartId__", this.chartOptions[0]);
+            	this.chartOptions[7] = String.format(this.chartOptions[7], "\'"+ this.chartDataSource + "\'");
+            	outputHTML = String.format(this.constructorTemplate, this.jsonEncode(this.chartOptions), fusionChartsEvents);
             }
         }
         return outputHTML;
+    }
+    
+    public void addEvent(String eventName, String funcName){
+    	String eventHTML;
+    	eventHTML = String.format(this.eventTemplate, eventName, funcName);
+    	this.fusionChartsEvents += eventHTML;
+    }
+    
+    public void addMessage(String messageName, String messageText){
+    	String messageString;
+    	messageString = String.format(this.messageTemplate, messageName, messageText);
+    	this.fusionChartsMessages += messageString;
     }
 }
